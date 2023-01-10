a07d160d6fdc2fc5e2005b0621012fff35adae02
whyisy <cjg0504@pusan.ac.kr>
2023-01-10 15:08:46 +0900

    feat: Add comparing user's guess with answer
    만약, 유저의 입력한 값이 정답과 같다면 축하메시지를 출력한다.
    그렇지 않: 경우 중,
    유저의 값이 정답보다 크다면, 크다는 메시지를,
    유저의 값이 정답보다 작다면, 작다는 메시지를 출력한다.

--- numguess.py
+++ numguess.py
@@ -1,5 +1,34 @@
-username = input("What is your name?")
-print(f"Hello, {username}!")
+from random import randint
+from time import sleep
 
-number = input("What number do you like?")
-print(f"you like {number}, right?")
+# Generate answer
+answer = randint(1,101)
+
+#Print answer for debugging
+print(answer)
+
+# User interaction
+username = input("Hi, there, What is your name?")
+print(f"Hi, {username}! Please be my guest!!")
+
+# Get and print User's guess
+guess = int(input(f"So {username}, Guess the number(1-100) : "))
+print(f'Well choice {username}~~ You picked {guess}!!')
+
+# Compare answer with user's guess
+if guess==answer:
+    print('************')
+    sleep(1)
+    print('************')
+    sleep(1)
+    print('************')
+    sleep(1)
+    print(f'You got it right!! The answer is {answer}!!')
+elif guess>answer:
+    print(f'Keep going, man~! That is too high, {username}')
+elif guess<answer:
+    print(f'Keep going, man~! That is too low, {username}')
+else:
+    print(f'Wrong answer! The answer was {answer}, {username}!!')
+
+
