4919fb70543292e45fa9d7865c0191730958b9ad
whyisy <cjg0504@pusan.ac.kr>
2023-01-11 10:58:24 +0900

    feat: Choose door randomly from doors
    store result for further use.

--- monty_hall.py
+++ monty_hall.py
@@ -1,5 +1,7 @@
-from randon import shuffle
+from random import shuffle, choice
 
 doors = [0,0,1] # 0: goat, 1: sports car
 shuffle(doors)
-print(doors)
+#print(doors)
+user_choice = choice(doors)
+print(user_choice)
