4cb5c847dc2809b6e06eff9db7ece73bdcb25e92
whyisy <cjg0504@pusan.ac.kr>
2023-01-11 18:20:15 +0900

    docs: Add commentary on monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -1,19 +1,25 @@
 from random import shuffle, choice
 
+# Set result dictionary
 result = {
         'stay_to_win': 0,
         'move_to_win': 0,
 }
-
+#Create doors with 0s and 1.
 doors = [0,0,1] # 0: goat, 1: sports car
 
+# Let User decide to running number
 iter_num = int(input("Enter some num(100-10000): "))
 
+# Start iteration
 for _ in range(iter_num):
+    # Shuffle doors
     shuffle(doors)
     #print(doors)
+    # Choose 1 random door
     user_choice = choice(doors)
     #print(user_choice)
+    # Count Winning possibility
     if user_choice==0:
         result['move_to_win'] += 1
     else:
--- numguess.py
+++ numguess.py
@@ -11,24 +11,46 @@ print(answer)
 username = input("Hi, there, What is your name?")
 print(f"Hi, {username}! Please be my guest!!")
 
-# Get and print User's guess
-guess = int(input(f"So {username}, Guess the number(1-100) : "))
-print(f'Well choice {username}~~ You picked {guess}!!')
-
 # Compare answer with user's guess
-if guess==answer:
-    print('************')
-    sleep(1)
-    print('************')
-    sleep(1)
-    print('************')
-    sleep(1)
-    print(f'You got it right!! The answer is {answer}!!')
-elif guess>answer:
-    print(f'Keep going, man~! That is too high, {username}')
-elif guess<answer:
-    print(f'Keep going, man~! That is too low, {username}')
-else:
-    print(f'Wrong answer! The answer was {answer}, {username}!!')
+count = int(input("How much trials do you want?"))
+point = 0
 
+def compare_ans(an,usrname,coun,poin):
+    if coun == 0:
+        if poin == 0 :
+            print('Your point is 0. you can get luck at another time')
+        print("Sorry, the game is over.")
+        return     
+        
+    else:
+        # Get and print User's guess
+        guess = int(input(f"So {username}, Guess the number(1-100) : "))
+        print(f'Well choice {username}~~ You picked {guess}!!')
+        
+        if guess==an:
+            print('************')
+            sleep(1)
+            print('************')
+            sleep(1)
+            print('************')
+            sleep(1)
+            poin += 1
+            print(f'You got it right!! The answer is {an}!!')
+            print(f"Your point is {poin}.")
+            an = randint(1,101)  #assign new an
+            print(an)   #Print answer for debugging
+        elif guess>an:
+            if coun == 1:
+                print(f'Wrong answer! The answer was {an}, {usrname}!!')
+            else:
+                print(f'Keep going, man~! That is too high, {usrname}')
+        elif guess<an:
+            if coun == 1:
+                print(f'Wrong answer! The answer was {an}, {usrname}!!')
+            else:
+                print(f'Keep going, man~! That is too low, {usrname}')
 
+        coun -= 1
+        compare_ans(an,usrname,coun,poin)
+
+compare_ans(answer,username,count,point)
